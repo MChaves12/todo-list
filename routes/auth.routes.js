828df4d3f872eb93d624ce9f9940ef4ce1d56510
MATHEUS CHAVES ARAUJO <002919@cesed.local>
2023-05-18 10:44:21 -0300

    Signup route created and authentications made

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,4 +8,32 @@ router.get('/', (req, res, next) => {
     res.json('Test route, all good');
 });
 
+router.post('/signup', async (req, res, next) => {
+    const { username, password } = req.body;
+    try {
+        if(!username || !password) {
+            res.status(400).json({message: 'Required fields'});
+            return;
+        }
+
+        const passwordRegex =  /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+        if(!passwordRegex.test(password)) {
+            res.status(400).json({message: 'The password must have 6 caracters and at least 1 number, 1 uppercase letter and 1 lowercase letter'});
+            return;
+        }
+
+        const user = await User.findOne({ username });
+        if(user) {
+            res.status(400).json({message: 'registered user. Try a new one'});
+            return;
+        }
+
+        const hash = bcrypt.hashSync(password, 10);
+        const newUser = await User.create({ username, passwordHash: hash });
+        res.status(201).json({ _id, username: newUser.username });
+    } catch (error) {
+        next(error);
+    }
+})
+
 module.exports = router;
